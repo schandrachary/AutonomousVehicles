6d7f066b266d4398aa92da1c0e64f98889d7574b
Shiva Chandrachary <shivchaitanya.vc@gmail.com>
2020-09-12 11:48:50 -0400

    indented the file for 4 spaces to a tab

--- CarND-Kidnapped-Vehicle-Project-master/src/particle_filter.cpp
+++ CarND-Kidnapped-Vehicle-Project-master/src/particle_filter.cpp
@@ -31,7 +31,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
    * NOTE: Consult particle_filter.h for more information about this method
    *   (and others in this file).
    */
-  num_particles = 50;  // TODO: Set the number of particles
+  num_particles = 25;  // TODO: Set the number of particles
 
   std::default_random_engine gen;
   std::normal_distribution<double> dist_x(x,std[0]);
@@ -63,23 +63,22 @@ void ParticleFilter::prediction(double delta_t, double std_pos[],
    *  http://www.cplusplus.com/reference/random/default_random_engine/
    */
     
-    // generate random noise
+    // generate random gaussian noise with mean 0 and std. deviation std_pos
     std::default_random_engine gen;
-    std::normal_distribution<double> dist_x(0,std_pos[0]);
-    std::normal_distribution<double> dist_y(0,std_pos[1]);
-    std::normal_distribution<double> dist_theta(0,std_pos[2]);
+    std::normal_distribution<double> dist_x(0.0,std_pos[0]);
+    std::normal_distribution<double> dist_y(0.0,std_pos[1]);
+    std::normal_distribution<double> dist_theta(0.0,std_pos[2]);
 
-   for(auto& particle : particles)
-   {
+    double normalizer = velocity/yaw_rate;
+    for(auto& particle : particles)
+    {
      // if the vehicle is not moving in the straight line
      if(fabs(yaw_rate) > 0.000001)
      {
-       double normalizer = velocity/yaw_rate;
        particle.x += normalizer*(std::sin(particle.theta + (yaw_rate*delta_t))
                                   - std::sin(particle.theta));
        particle.y += normalizer*(std::cos(particle.theta)
                                   - std::cos(particle.theta + (yaw_rate*delta_t)));
-
        particle.theta += (yaw_rate*delta_t);
      }
 
@@ -88,19 +87,13 @@ void ParticleFilter::prediction(double delta_t, double std_pos[],
      {
        particle.x += velocity*delta_t*std::cos(particle.theta);
        particle.y += velocity*delta_t*std::sin(particle.theta);
-
-       // generate random noise
-       std::default_random_engine gen;
-       std::normal_distribution<double> dist_x(particle.x,std_pos[0]);
-       std::normal_distribution<double> dist_y(particle.y,std_pos[1]);
-       std::normal_distribution<double> dist_theta(particle.theta,std_pos[2]);
      }
        
        // add noise to particle pose
        particle.x += dist_x(gen);
        particle.y += dist_y(gen);
        particle.theta += dist_theta(gen);
-   }
+    }
 
 }
 
@@ -134,80 +127,81 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
    *   (look at equation 3.33) http://planning.cs.uiuc.edu/node99.html
    */
 
-   int iParticle = 0;
-   for(auto& particle : particles)
-   {
-     // perform data association
-     vector<LandmarkObs> temp_observations = observations;
-     vector<LandmarkObs> associated_landmark(observations.size());
-     particle.weight = 1;
-     int iObservation = 0;
-     for(auto& observation : temp_observations)
-     {
-       // convert each observation from vehicle coordinate system to global space
-       observation.x = particle.x + (std::cos(particle.theta)*observation.x)
-                                       - (std::sin(particle.theta)*observation.y);
-       observation.y = particle.y + (std::sin(particle.theta)*observation.x)
-                                       + (std::cos(particle.theta)*observation.y);
+    int iParticle = 0;
+    for(auto& particle : particles)
+    {
+        // perform data association
+        vector<LandmarkObs> temp_observations = observations;
+        vector<LandmarkObs> associated_landmark(observations.size());
+        particle.weight = 1;
+        int iObservation = 0;
+        
+        for(auto& observation : temp_observations)
+        {
+            // convert each observation from vehicle coordinate system to global coordinates
+            observation.x = particle.x + (std::cos(particle.theta)*observation.x)
+                                           - (std::sin(particle.theta)*observation.y);
+            observation.y = particle.y + (std::sin(particle.theta)*observation.x)
+                                           + (std::cos(particle.theta)*observation.y);
+            // initialize observation id
+            observation.id = -1;
+             
+            double shortest_distance = std::numeric_limits<double>::infinity();
+            for(const auto& landmark : map_landmarks.landmark_list)
+            {
+                double distance = dist(landmark.x_f, landmark.y_f, observation.x, observation.y);
+                double distance_to_landmark = dist(particle.x, particle.y, landmark.x_f, landmark.y_f);
 
+                 // assign the landmark id of shortest_distance to observation
+                if(distance < shortest_distance && distance_to_landmark < sensor_range)
+                {
+                    shortest_distance = distance;
+                    observation.id = landmark.id_i;
 
-       // initialize observation id
-       observation.id = -1;
-       double shortest_distance = std::numeric_limits<double>::infinity();
-       for(auto& landmark : map_landmarks.landmark_list)
-       {
-         double distance = dist(landmark.x_f, landmark.y_f, observation.x, observation.y);
-         double distance_to_landmark = dist(particle.x, particle.y, landmark.x_f, landmark.y_f);
+                    // store this landmark as an associated landmark
+                    associated_landmark.at(iObservation).id = landmark.id_i;
+                    associated_landmark.at(iObservation).x = landmark.x_f;
+                    associated_landmark.at(iObservation).y = landmark.y_f;
+                    //std::cout << "Distance to particle "<< iParticle << " is: "<<distance;
+                }
+             //std::cout << " Shortest distance for observation: "<< iObservation<< " is: " << shortest_distance <<std::endl;
+            }
+            
 
-         // assign the landmark id of shortest_distance to observation
-         if(distance < shortest_distance && distance_to_landmark < sensor_range)
-         {
-           shortest_distance = distance;
-           observation.id = landmark.id_i;
+            // throw an error if the observation id is not updated
+            assert(observation.id != -1);
 
-           // store this landmark as an associated landmark
-           associated_landmark.at(iObservation).id = landmark.id_i;
-           associated_landmark.at(iObservation).x = landmark.x_f;
-           associated_landmark.at(iObservation).y = landmark.y_f;
-           //std::cout << "Distance to particle "<< iParticle << " is: "<<distance;
-         }
-         //std::cout << " Shortest distance for observation: "<< iObservation<< " is: " << shortest_distance <<std::endl;
-       }
+            // update the weight of the particle with the landmark and observation
+            assert(observation.id == associated_landmark.at(iObservation).id);
+            particle.weight *= multiv_prob(std_landmark[0], std_landmark[1],
+                               observation.x, observation.y,
+                               associated_landmark.at(iObservation).x,
+                               associated_landmark.at(iObservation).y);
 
-       // throw an error if the observation id is not updated
-       assert(observation.id != -1);
+            ++iObservation;
+        }
+        
+        //Update the weights vector
+        weights.at(iParticle) = particle.weight;
+        std::cout << "Weight for particle "<< iParticle << " is: "<< particle.weight << std::endl;
 
-       // update the weight of the particle with the landmark and observation
-         assert(observation.id == associated_landmark.at(iObservation).id);
-        particle.weight *= multiv_prob(std_landmark[0], std_landmark[1],
-                                   observation.x, observation.y,
-                                   associated_landmark.at(iObservation).x,
-                                   associated_landmark.at(iObservation).y);
+        // store the associated-landmark in vectors and set the association for each particle
+        vector<int> associations(observations.size());
+        vector<double> sense_x(observations.size());
+        vector<double> sense_y(observations.size());
+        int i = 0;
+        for(auto landmark : associated_landmark)
+        {
+            associations.at(i) = landmark.id;
+            sense_x.at(i) = landmark.x;
+            sense_y.at(i) = landmark.y;
+            i++;
+        }
+        SetAssociations(particle, associations, sense_x, sense_y);
 
-        ++iObservation;
-     }
-
-     //Update the weights vector
-     weights.at(iParticle) = particle.weight;
-     std::cout << "Weight for particle "<< iParticle << " is: "<< particle.weight << std::endl;
-
-     // store the associated-landmark in vectors and set the association for each particle
-     vector<int> associations(observations.size());
-     vector<double> sense_x(observations.size());
-     vector<double> sense_y(observations.size());
-       int i = 0;
-     for(auto landmark : associated_landmark)
-     {
-       associations.at(i) = landmark.id;
-       sense_x.at(i) = landmark.x;
-       sense_y.at(i) = landmark.y;
-         i++;
-     }
-     SetAssociations(particle, associations, sense_x, sense_y);
-
-     // increment iParticle
-     ++iParticle;
-   }
+        // increment iParticle
+        ++iParticle;
+    }
 }
 
 void ParticleFilter::resample() {
@@ -218,17 +212,24 @@ void ParticleFilter::resample() {
    *   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
    */
 
-   std::default_random_engine gen;
-   std::discrete_distribution<long> dist_weights(weights.begin(), weights.end());
-   std::vector<Particle> resampled_particles(particles.size());
-   for(auto iParticle=0; iParticle<particles.size(); ++iParticle)
-   {
-       auto weight_chosen = dist_weights(gen);
-       std::cout << "Chosen weight: " << weight_chosen << std::endl;
-     resampled_particles.at(iParticle) = particles.at(weight_chosen);
-   }
-   particles = resampled_particles;
-
+    std::default_random_engine gen;
+    std::discrete_distribution<long> dist_weights(weights.begin(), weights.end());
+    std::vector<Particle> resampled_particles(particles.size());
+    
+    for(auto iParticle=0; iParticle<particles.size(); ++iParticle)
+    {
+        auto weight_chosen = dist_weights(gen);
+        std::cout << "Chosen weight: " << weight_chosen << std::endl;
+        resampled_particles.at(iParticle) = particles.at(weight_chosen);
+    }
+    particles = resampled_particles;
+//    std::cout << "Weights of chosen particles: " << std::endl;
+//    int i = 0;
+//    for(const auto& particle : particles)
+//    {
+//        std::cout << i << ". " << particle.weight << std::endl;
+//        i++;
+//    }
 }
 
 void ParticleFilter::SetAssociations(Particle& particle,
