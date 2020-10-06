132bcc57aec06b595d90b547831fcd7d32b54eb7
Shiva Chandrachary <shivchaitanya.vc@gmail.com>
2020-10-06 06:58:15 -0400

    created PID based control for steering activation

---
+++ CarND-PID-Control-Project/src/PID.cpp
@@ -0,0 +1,42 @@
+#include "PID.h"
+
+/**
+ * TODO: Complete the PID class. You may add any additional desired functions.
+ */
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp_, double Ki_, double Kd_) {
+  /**
+   * TODO: Initialize PID coefficients (and errors, if needed)
+   */
+   Kp = Kp_;
+   Ki = Ki_;
+   Kd = Kd_;
+
+   p_error = 0;
+   d_error = 0;
+   i_error = 0;
+}
+
+void PID::UpdateError(double cte) {
+  /**
+   * TODO: Update PID errors based on cte.
+   */
+
+   p_error = cte;
+   d_error = cte - prev_cte;
+   i_error += cte;
+   prev_cte = cte;
+
+}
+
+double PID::TotalError() {
+  /**
+   * TODO: Calculate and return the total error
+   */
+
+   return -Kp*p_error - Kd*d_error - Ki*i_error;
+}
