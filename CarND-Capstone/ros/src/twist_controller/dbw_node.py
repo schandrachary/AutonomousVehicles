4c6c6032a11c64bd4364fdd555104d30352b5fb7
Shiva Chandrachary <shivchaitanya.vc@gmail.com>
2020-10-31 07:44:15 -0700

    Use member variables

--- CarND-Capstone/ros/src/twist_controller/dbw_node.py
+++ CarND-Capstone/ros/src/twist_controller/dbw_node.py
@@ -86,13 +86,13 @@ class DBWNode(object):
             # TODO: Get predicted throttle, brake, and steering using `twist_controller`
             # You should only publish the control commands if dbw is enabled
             if not None in (self.current_vel, self.linear_vel, self.angular_vel):
-                self.throttle, self.brake, self.steering = self.controller.control(self.current_vel,
+                self.throttle, self.brake, self.steer = self.controller.control(self.current_vel,
                                                                             self.dbw_enabled,
                                                                             self.linear_vel,
                                                                             self.angular_vel)
 
-            if dbw is enabled:
-              self.publish(throttle, brake, steer)
+            if self.dbw_enabled:
+              self.publish(self.throttle, self.brake, self.steer)
             rate.sleep()
 
     def dbw_enabled_cb(self, msg):
