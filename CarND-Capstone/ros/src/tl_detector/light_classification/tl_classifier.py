b0200c6bc07b856c040dfcc89346f2cd0d3850b1
Shiva Chandrachary <shivchaitanya.vc@gmail.com>
2020-11-06 17:45:53 -0500

    Traffic light classification using image processing

--- CarND-Capstone/ros/src/tl_detector/light_classification/tl_classifier.py
+++ CarND-Capstone/ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,4 +1,6 @@
 from styx_msgs.msg import TrafficLight
+import cv2
+import numpy as np 
 
 class TLClassifier(object):
     def __init__(self):
@@ -16,4 +18,33 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
+
+        # Convert the image to HSV color space
+        image_hsv = cv2.cvtColor(image,cv2.COLOR_BGR2HSV)
+
+        # Create a threshold for RED pixels in the image
+        RED_MIN = np.array([0, 120, 120], np.uint8)
+        RED_MAX = np.array([10, 255, 255], np.uint8)
+        threshold = cv2.inRange(image_hsv, RED_MIN, RED_MAX)
+        red_pixels = cv2.countNonZero(threshold)
+        if red_pixels > 60:
+            return TrafficLight.RED
+
+        # Create a threshold for YELLOW pixels in the image
+        YELLOW_MIN = np.array([28, 120, 120], np.uint8)
+        YELLOW_MAX = np.array([47, 255, 255], np.uint8)
+        threshold = cv2.inRange(image_hsv, YELLOW_MIN, YELLOW_MAX)
+        yellow_pixels = cv2.countNonZero(threshold)
+        if yellow_pixels > 60:
+            return TrafficLight.YELLOW
+
+        # Create a threshold for GREEN pixels in the image
+        GREEN_MIN = np.array([64, 120, 120], np.uint8)
+        GREEN_MAX = np.array([100, 255, 255], np.uint8)
+        threshold = cv2.inRange(image_hsv, GREEN_MIN, GREEN_MAX)
+        green_pixels = cv2.countNonZero(threshold)
+        if green_pixels > 60:
+            return TrafficLight.GREEN
+
+
         return TrafficLight.UNKNOWN
